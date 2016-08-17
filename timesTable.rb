72722e2c81cd25019dccb383446b2d7fd1157d65
James Youn <jamesyoun710@gmail.com>
2016-08-16 22:51:01 -0700

    (main) Add #prime_array

--- timesTable.rb
+++ timesTable.rb
@@ -5,13 +5,27 @@ class TimesTable
 
   def is_prime?(target_number)
     max_check = Math.sqrt(target_number)
-    puts max_check
 
     2.upto(max_check) do |check_num|
       return false if target_number % check_num == 0
     end
 
     true
+  end
+
+  def prime_array(n_primes)
+    result_arr = []
+    current_num = 2
+
+    while result_arr.length < n_primes do
+      if(is_prime?(current_num))
+        result_arr.push(current_num)
+      end
+
+      current_num += 1
+    end
+
+    result_arr
   end
 end
 
