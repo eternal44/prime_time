f0e3318e851b6237898c605a06159714b1a17d66
James Youn <jamesyoun710@gmail.com>
2016-08-16 22:44:07 -0700

    (main) Add is_prime? method

--- timesTable.rb
+++ timesTable.rb
@@ -1,5 +1,16 @@
 class TimesTable
   def something
+    puts ARGV[0]
+  end
+
+  def is_prime?(target_number)
+    max_check = Math.sqrt(target_number)
+    puts max_check
+
+    2.upto(max_check) do |check_num|
+      return false if target_number % check_num == 0
+    end
+
     true
   end
 end
